50f0ed8ae1215498805bdecafef53d10037d4ca8
Vlad Chursin <chursin.v@gmail.com>
2017-05-10 13:13:55 +0300

    test

---
+++ install.bat
@@ -0,0 +1,6 @@
+rem Download and install https://conda.io/miniconda.html
+rem git clone https://github.com/chshkhr/tw2sf.git
+python -m pip install python-dateutil
+python -m pip install requests
+python -m pip install pymysql
+python -m pip install --upgrade ShopifyAPI 
